29175fa8ed7de30f9c4c92e6dc68368be5667761
Michael Pope <mpope.cr@gmail.com>
2018-01-09 15:32:16 -0800

    Delete unused package.head.exs

--- package.head.exs
+++
@@ -1,7 +0,0 @@
-Expm.Package.new(
-  name: "flager",
-  description: "Elixir wrapper over basho/lager",
-  version: :head,
-  keywords: ["Elixir","Erlang","logging","lager"],
-  maintainers: [[name: "ILYA Khlopotov", email: "ilya.khlopotov@gmail.com"]],
-  repositories: [[github: "khia/flager"]])
