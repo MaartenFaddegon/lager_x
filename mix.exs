81a59389f2a3b62c8620116eed0e86f709fa773f
Michael Pope <mpope.cr@gmail.com>
2018-01-09 13:04:23 -0800

    Add ex_doc dependency

--- mix.exs
+++ mix.exs
@@ -22,6 +22,9 @@ defmodule Flager.Mixfile do
 
   defp deps do
     [
+      # doc generator
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      # logging library
       {:lager, git: "https://github.com/basho/lager.git", tag: "3.2.4"},
     ]
   end
