c36edec9ebc4250ddafdcbc006ee28271c67bc1d
Michael Pope <mpope.cr@gmail.com>
2018-01-17 16:40:06 -0800

    Add a passthrough backend

---
+++ lib/lager_x/passthrough_backend.ex
@@ -0,0 +1,196 @@
+defmodule :lager_x_passthrough_backend do
+  @behaviour :gen_event
+
+  defstruct [
+    lager_util_is_loggable: &:lager_util.is_loggable/3,
+    lager_util_level_to_num: &:lager_util.level_to_num/1,
+    log_level_number: nil,
+    passthrough_init_arg: nil,
+    passthrough_module: nil,
+    passthrough_state: nil,
+  ]
+
+  @type call :: :get_loglevel | {:set_loglevel,log_level()} | term()
+  @type event :: {:log,message :: :lager_msg.lager_msg()} | term()
+  @type init_opts :: [{:passthrough_module,passthrough_module()}|{:passthrough_init_arg,term()},...]
+  @type lager_util_is_loggable :: (
+    message :: :lager_msg.lager_msg(),
+    log_level_number(),
+    __MODULE__ -> boolean()
+  )
+  @type lager_util_level_to_num :: (log_level()->log_level_number())
+  @type log_level :: :debug|:info|:notice|:warning|:error|
+                     :critical|:alert|:emergency|:none
+  @type log_level_number :: non_neg_integer()|{:mask, non_neg_integer()}
+  @type on_handle_call :: {:ok,log_level_number(),state()} | {:ok,:ok,state()} | {:ok,reply :: term(),state()} | {:ok,reply :: term(),state(),:hibernate} | {:swap_handler,reply :: term(),args1 :: term(),state(),handler2 :: module(),args2 :: term()} | {:remove_handler,reply :: term()}
+  @type on_handle_event :: {:ok,state()} | {:ok,state(),:hibernate} | {:swap_handler,args1 :: term(),state(),handler2 :: module(),args2 :: term()} | :remove_handler
+  @type on_handle_info :: {:ok,state()} | {:ok,state(),:hibernate} | {:swap_handler,args1 :: term(),state(),handler2 :: module(),args2 :: term()} | :remove_handler
+  @type on_init :: {:ok,state()} | {:ok,state,:hibernate} | {:error,reason :: term()}
+  @type on_terminate :: {reason :: term()}
+  @type passthrough_init_arg :: term()
+  @type passthrough_module :: module()
+  @type passthrough_state :: term()
+  @type state :: %__MODULE__{
+    lager_util_is_loggable: lager_util_is_loggable(),
+    lager_util_level_to_num: lager_util_level_to_num(),
+    log_level_number: log_level_number,
+    passthrough_init_arg: passthrough_init_arg,
+    passthrough_module: passthrough_module,
+    passthrough_state: passthrough_state,
+  }
+
+  #############
+  # Callbacks #
+  #############
+
+  @spec handle_call(call(),state()) :: on_handle_call()
+  def handle_call(call,state)
+
+  def handle_call(:get_loglevel,%__MODULE__{}=state) do
+    {:ok,state.log_level_number,state}
+  end
+
+  def handle_call({:set_loglevel,log_level},%__MODULE__{}=state) do
+    state.lager_util_level_to_num
+    |> apply([log_level])
+    |> case do
+      log_level_number -> struct!(state,log_level_number: log_level_number)
+    end
+    |> case do
+      state2 -> {:ok,:ok,state2}
+    end
+  end
+
+  def handle_call(call,%__MODULE__{}=state) do
+    state.passthrough_module
+    |> apply(:handle_call,[call,state.passthrough_state])
+    |> case do
+      {:ok,reply,passthrough_state} ->
+        state
+        |> struct!(passthrough_state: passthrough_state)
+        |> case do
+          state2 -> {:ok,reply,state2}
+        end
+      {:ok,reply,passthrough_state,:hibernate} ->
+        state
+        |> struct!(passthrough_state: passthrough_state)
+        |> case do
+          state2 -> {:ok,reply,state2,:hibernate}
+        end
+      {:swap_handler,reply,args1,passthrough_state,handler2,args2} ->
+        state
+        |> struct!(passthrough_state: passthrough_state)
+        |> case do
+          state2 -> {:swap_handler,reply,args1,state2,handler2,args2}
+        end
+      {:remove_handler,_reply}=on_remove_handler ->
+        on_remove_handler
+    end
+  end
+
+  @spec handle_event(event(),state()) :: on_handle_event()
+  def handle_event(event,state)
+
+  def handle_event({:log,lager_message}=event,%__MODULE__{}=state) do
+    state.lager_util_is_loggable
+    |> apply([lager_message,state.log_level_number,__MODULE__])
+    |> if do
+      state.passthrough_module
+      |> apply(:handle_event,[event,state.passthrough_state])
+      |> case do
+        {:ok,passthrough_state} ->
+          state
+          |> struct!(passthrough_state: passthrough_state)
+          |> case do
+            state2 -> {:ok,state2}
+          end
+        {:ok,passthrough_state,:hibernate} ->
+          state
+          |> struct!(passthrough_state: passthrough_state)
+          |> case do
+            state2 -> {:ok,state2,:hibernate}
+          end
+        {:swap_handler,args1,passthrough_state,handler2,args2} ->
+          state
+          |> struct!(passthrough_state: passthrough_state)
+          |> case do
+            state2 -> {:swap_handler,args1,state2,handler2,args2}
+          end
+        :remove_handler=on_remove_handler ->
+          on_remove_handler
+      end
+    else
+      {:ok,state}
+    end
+  end
+
+  def handle_event(_,state) do
+    {:ok,state}
+  end
+
+  @spec handle_info(info :: term(),state()) :: on_handle_info()
+  def handle_info(info,state)
+
+  def handle_info(info,%__MODULE__{}=state) do
+    state.passthrough_module
+    |> apply(:handle_info,[info,state.passthrough_state])
+    |> case do
+      {:ok,passthrough_state} ->
+        state
+        |> struct!(passthrough_state: passthrough_state)
+        |> case do
+          state2 -> {:ok,state2}
+        end
+      {:ok,passthrough_state,:hibernate} ->
+        state
+        |> struct!(passthrough_state: passthrough_state)
+        |> case do
+          state2 -> {:ok,state2,:hibernate}
+        end
+      {:swap_handler,args1,passthrough_state,handler2,args2} ->
+        state
+        |> struct!(passthrough_state: passthrough_state)
+        |> case do
+          state2 -> {:swap_handler,args1,state2,handler2,args2}
+        end
+      :remove_handler=on_remove_handler ->
+        on_remove_handler
+    end
+  end
+
+  @spec init(init_opts()) :: on_init()
+  def init(opts) do
+    _ = Keyword.fetch!(opts,:passthrough_init_arg)
+    _ = Keyword.fetch!(opts,:passthrough_module)
+
+    __MODULE__
+    |> struct!(opts)
+    |> case do
+      state ->
+        state.passthrough_module
+        |> apply(:init,[state.passthrough_init_arg])
+        |> case do
+          {:ok,passthrough_state} ->
+            state
+            |> struct!(passthrough_state: passthrough_state)
+            |> case do
+              state2 -> {:ok,state2}
+            end
+          {:ok,passthrough_state,:hibernate} ->
+            state
+            |> struct!(passthrough_state: passthrough_state)
+            |> case do
+              state2 -> {:ok,state2,:hibernate}
+            end
+          {:error,_reason}=on_error ->
+            on_error
+      end
+    end
+  end
+
+  @spec terminate(reason :: term(),state()) :: on_terminate()
+  def terminate(reason,state) do
+    state.passthrough_module
+    |> apply(:terminate,[reason,state.passthrough_state])
+  end
+end
---
+++ test/lager_x/passthrough_backend_test.exs
@@ -0,0 +1,315 @@
+defmodule LagerX.PassthroughBackendTest do
+  use ExUnit.Case, async: false
+
+  defmodule Passthrough do
+    def handle_call(call,state) do
+      state2 = Map.put(state,:last_message,call)
+
+      case call do
+        ref when is_reference(ref) ->
+          {:ok,call,state2}
+        {:hibernate,_} ->
+          {:ok,call,state2,:hibernate}
+        {:swap_handler,args1,handler2,args2} ->
+          {:swap_handler,call,args1,state2,handler2,args2}
+        {:remove_handler,_} ->
+          {:remove_handler,call}
+      end
+    end
+
+    def handle_event({:log,test_message}=event,state) do
+      state2 = Map.put(state,:last_message,event)
+
+      case test_message do
+        ref when is_reference(ref) ->
+          {:ok,state2}
+        {:hibernate,_event} ->
+          {:ok,state2,:hibernate}
+        {:swap_handler,ref1,handler2,ref2} ->
+          {:swap_handler,ref1,state2,handler2,ref2}
+        :remove_handler ->
+          :remove_handler
+        other ->
+          raise "invalid: #{inspect(other)}"
+      end
+    end
+
+    def handle_info(info,state) do
+      state2 = Map.put(state,:last_message,info)
+
+      case info do
+        ^info when is_reference(info) ->
+          {:ok,state2}
+        {:hibernate,_} ->
+          {:ok,state2,:hibernate}
+        {:swap_handler,args1,handler2,args2} ->
+          {:swap_handler,args1,state2,handler2,args2}
+        :remove_handler ->
+          :remove_handler
+      end
+    end
+
+    def init(arg) do
+      state = %{last_message: arg}
+      case arg do
+        ^arg when is_reference(arg) ->
+          {:ok,state}
+        {:hibernate,_} ->
+          {:ok,state,:hibernate}
+        {:error,_} ->
+          {:error,arg}
+      end
+    end
+
+    def terminate(reason,state) do
+      state2 = Map.put(state,:last_message,reason)
+      {reason,state2}
+    end
+  end
+
+  def expected_state(%:lager_x_passthrough_backend{}=state,message) do
+    passthrough_state2 = %{last_message: message}
+    struct!(state,passthrough_state: passthrough_state2)
+  end
+
+  def mock_lager_event(event) do
+    {:log,event}
+  end
+
+  def set_log_level_number() do
+    123
+  end
+
+  def state() do
+    %:lager_x_passthrough_backend{
+      lager_util_is_loggable: fn (_,_,_) -> true end,
+      lager_util_level_to_num: fn (_) -> set_log_level_number() end,
+      log_level_number: 3,
+      passthrough_module: Passthrough,
+      passthrough_state: %{},
+    }
+  end
+
+  setup do
+    []
+    |> Keyword.put(:passthrough_module,Passthrough)
+    |> Keyword.put(:set_log_level,:fake_level)
+    |> Keyword.put(:set_log_level_number,set_log_level_number())
+    |> Keyword.put(:state,state())
+    |> case do
+     context -> {:ok,context}
+    end
+  end
+
+  describe "&handle_call/2" do
+    test "get_loglevel case",c do
+      call = :get_loglevel
+      actual = :lager_x_passthrough_backend.handle_call(call,c.state)
+      expected = {:ok,c.state.log_level_number,c.state}
+      assert actual == expected
+    end
+
+    test "set_loglevel case",c do
+      call = {:set_loglevel,c.set_log_level}
+      actual = :lager_x_passthrough_backend.handle_call(call,c.state)
+      expected =
+        c.state
+        |> struct(log_level_number: c.set_log_level_number)
+        |> case do
+          state -> {:ok,:ok,state}
+        end
+      assert actual == expected
+    end
+
+    test "stadard case",c do
+      call = make_ref()
+      actual = :lager_x_passthrough_backend.handle_call(call,c.state)
+      expected = {:ok,call,expected_state(c.state,call)}
+      assert actual == expected
+    end
+
+    test "hibernate case",c do
+      call = {:hibernate,make_ref()}
+      actual = :lager_x_passthrough_backend.handle_call(call,c.state)
+      expected = {:ok,call,expected_state(c.state,call),:hibernate}
+      assert actual == expected
+    end
+
+    test "swap_handler case",c do
+      args1 = [make_ref()]
+      handler2 = :fake_handler
+      args2 = [make_ref()]
+      call = {:swap_handler,args1,handler2,args2}
+      actual = :lager_x_passthrough_backend.handle_call(call,c.state)
+      expected = {
+        :swap_handler,
+        call,
+        args1,
+        expected_state(c.state,call),
+        handler2,
+        args2
+      }
+      assert actual == expected
+    end
+
+    test "remove_handler case",c do
+      call = {:remove_handler,make_ref()}
+      actual = :lager_x_passthrough_backend.handle_call(call,c.state)
+      expected = {:remove_handler,call}
+      assert actual == expected
+    end
+  end
+
+  describe "&handle_event/2" do
+    test "standard case",c do
+      event = make_ref() |> mock_lager_event()
+      actual = :lager_x_passthrough_backend.handle_event(event,c.state)
+      expected = {:ok,expected_state(c.state,event)}
+      assert actual == expected
+    end
+
+    test "hibernate case",c do
+      event = {:hibernate,make_ref()} |> mock_lager_event()
+      actual = :lager_x_passthrough_backend.handle_event(event,c.state)
+      expected = {:ok,expected_state(c.state,event),:hibernate}
+      assert actual == expected
+    end
+
+    test "swap_handler case",c do
+      args1 = [make_ref()]
+      handler2 = :fake_handler
+      args2 = [make_ref()]
+      event = {:swap_handler,args1,handler2,args2}|> mock_lager_event()
+      actual = :lager_x_passthrough_backend.handle_event(event,c.state)
+      expected = {
+        :swap_handler,
+        args1,
+        expected_state(c.state,event),
+        handler2,
+        args2
+      }
+      assert actual == expected
+    end
+
+    test "remove_handler case",c do
+      event = :remove_handler |> mock_lager_event()
+      actual = :lager_x_passthrough_backend.handle_event(event,c.state)
+      expected = :remove_handler
+      assert actual == expected
+    end
+
+    test "non lager event case",c do
+      event = :non_log_event
+      actual = :lager_x_passthrough_backend.handle_event(event,c.state)
+      expected = {:ok,c.state}
+      assert actual == expected
+    end
+  end
+
+  describe "&handle_info/2" do
+    test "standard path",c do
+      info = make_ref()
+      actual = :lager_x_passthrough_backend.handle_info(info,c.state)
+      expected = {:ok,expected_state(c.state,info)}
+      assert actual == expected
+    end
+
+    test "hibernate path",c do
+      info = {:hibernate,make_ref()}
+      actual = :lager_x_passthrough_backend.handle_info(info,c.state)
+      expected = {:ok,expected_state(c.state,info),:hibernate}
+      assert actual == expected
+    end
+
+    test "swap_handler path",c do
+      args1 = [make_ref()]
+      handler2 = :fake_handler
+      args2 = [make_ref()]
+      info = {:swap_handler,args1,handler2,args2}
+      actual = :lager_x_passthrough_backend.handle_info(info,c.state)
+      expected = {
+        :swap_handler,
+        args1,
+        expected_state(c.state,info),
+        handler2,
+        args2
+      }
+      assert actual == expected
+    end
+
+    test "remove_handler path",c do
+      info = :remove_handler
+      actual = :lager_x_passthrough_backend.handle_info(info,c.state)
+      expected = :remove_handler
+      assert actual == expected
+    end
+  end
+
+  describe "&init/1" do
+    test "missing passthrough_module case" do
+      opts = [passthrough_init_arg: []]
+      func = fn -> :lager_x_passthrough_backend.init(opts) end
+      assert_raise KeyError, func
+    end
+
+    test "missing passthrough_init_arg case",c do
+      opts = [passthrough_module: c.passthrough_module]
+      func = fn -> :lager_x_passthrough_backend.init(opts) end
+      assert_raise KeyError, func
+    end
+
+    test "standard path",c do
+      arg = make_ref()
+      opts = [
+        passthrough_init_arg: arg,
+        passthrough_module: c.passthrough_module,
+      ]
+      actual = :lager_x_passthrough_backend.init(opts)
+      expected =
+        opts
+        |> case do
+          opts2 -> struct!(:lager_x_passthrough_backend,opts2)
+        end
+        |> case do
+          state -> {:ok,expected_state(state,arg)}
+        end
+      assert actual == expected
+    end
+
+    test "hibernate path",c do
+      arg = {:hibernate,make_ref()}
+      opts = [
+        passthrough_init_arg: arg,
+        passthrough_module: c.passthrough_module,
+      ]
+      actual = :lager_x_passthrough_backend.init(opts)
+      expected =
+        opts
+        |> case do
+          opts2 -> struct!(:lager_x_passthrough_backend,opts2)
+        end
+        |> case do
+          state -> {:ok,expected_state(state,arg),:hibernate}
+        end
+      assert actual == expected
+    end
+
+    test "error path",c do
+      arg = {:error,make_ref()}
+      opts = [
+        passthrough_init_arg: arg,
+        passthrough_module: c.passthrough_module,
+      ]
+      actual = :lager_x_passthrough_backend.init(opts)
+      expected = {:error,arg}
+      assert actual == expected
+    end
+  end
+
+  test "&terminate/2",c do
+    reason = make_ref()
+    actual = :lager_x_passthrough_backend.terminate(reason,c.state)
+    expected = {reason,%{last_message: reason}}
+    assert actual == expected
+  end
+end
